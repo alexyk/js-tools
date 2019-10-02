39824b511cec2c3cb5f4abd5f2e3b7b32190b053
alexyk <370500+alexyk@users.noreply.github.com>
2019-10-02 19:19:35 +0300

    Release v0.1.1 - updating to changes in es5

--- src/index.js
+++ src/index.js
@@ -162,6 +162,10 @@ function isObjectEmpty(object) {
 
 
 function isObject(value, className = null) {
+  if (value == null) {
+    return false;
+  }
+
   let result = typeof value == "object";
   if (!result) {
     className = className ? className : getObjectClassName(value);
@@ -308,6 +312,5 @@ export {
   config, setConfig, LOG_TYPE, measureTime,
   log, dlog, logGreen, logWarn, logError, 
   getObjectClassName, getObjectFromPath, getConditionsByPath, getObjectKeysCount,
-  isString, isNumber, isSymbol, isObjectEmpty, isObject, isArray, isFunction,
-  callWithArgs
+  isString, isNumber, isSymbol, isObjectEmpty, isObject, isArray, isFunction
 }
